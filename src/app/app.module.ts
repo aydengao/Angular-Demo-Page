7a12cf7cd83ede51866fde42f5bc3f7bb4e87aff
unknown <gao.ayden@gmail.com>
2023-10-16 20:42:12 -0700

    added HousingLocation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HomeComponent} from "./home/home.component";
 
+// A component module is needed if a component is not marked standalone
+// A component cannot be standalone and have its own module
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  // declarations: [
+  //   AppComponent
+  // ],
     imports: [
         BrowserModule,
         HomeComponent
     ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from "@angular/platform-browser";
 
+// Creating page based on module
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// Creating page based on component
+bootstrapApplication(AppComponent);
