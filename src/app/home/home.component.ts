80a03000e1f7fa860a023a57c5ee8b2bea9d24b6
Ayden <gao.ayden@gmail.com>
2023-10-16 21:09:44 -0700

    Added HousingLocation.ts interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent} from "../housing-location/housing-location.component";
+import { HousingLocation } from '../housing-location';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -25,5 +26,16 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
---
+++ src/app/housing-location.ts
@@ -0,0 +1,11 @@
+// we create an interface with said fields below
+export interface HousingLocation {
+  id: number;
+  name: string;
+  city: string;
+  state: string;
+  photo: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+}
